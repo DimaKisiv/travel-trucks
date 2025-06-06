2fce63a4b2ce131534409cece9af44e847bfded5
dimakisiv <dimakisiv@gmail.com>
2025-06-07 00:25:17 +0300

    navigation and homepage banner styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,11 +4,13 @@ import styles from './Banner.module.css';
 function Banner() {
   return (
     <div className={styles.banner}>
-      <h1>Welcome to Travel Trucks</h1>
-      <p>Explore our amazing campers!</p>
-      <Link to="/campers" className={styles.button}>
-        View Campers
-      </Link>
+      <div className={styles.bannerContent}>
+        <h1 className={styles.headline}>Campers of your dreams</h1>
+        <h2 className={styles.subHeadline}>You can find everything you want in our catalog</h2>
+        <Link to="/campers" className={styles.viewMoreBtn}>
+          View Now
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,43 @@
-import { Link } from 'react-router';
-import styles from './Navigation.module.css';
+import { NavLink } from "react-router";
+import styles from "./Navigation.module.css";
+import Logo from "../../assets/img/Logo.svg";
 
 function Navigation() {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/campers">Campers</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <img
+            src={Logo}
+            alt="Travel Trucks Logo"
+            className={styles.logoImage}
+          />
+        </div>
+        <ul className={styles.centerNav}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? `${styles.navBtn} ${styles.active}` : styles.navBtn
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/campers"
+              className={({ isActive }) =>
+                isActive ? `${styles.navBtn} ${styles.active}` : styles.navBtn
+              }
+            >
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
